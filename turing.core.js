c5625f82ccd6f18df5be70f7f5c102de9fc908cb
Alex Young <alex@helicoid.net>
2011-05-19 19:14:42 +0100

    Updated the version in turing.core

--- turing.core.js
+++ turing.core.js
@@ -25,8 +25,8 @@
     }
   }
 
-  turing.VERSION = '0.0.62';
-  turing.lesson = 'Part 62: Attributes';
+  turing.VERSION = '0.0.63';
+  turing.lesson = 'Part 63: Attributes';
 
   /**
    * This alias will be used as an alternative to `turing()`.
