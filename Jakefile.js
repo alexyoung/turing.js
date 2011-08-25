e62ce66e1caaf2ee3e6ec8842075203df8c52e5a
Alex Young <alex@helicoid.net>
2011-08-25 18:09:01 +0100

    Updated docs

--- Jakefile.js
+++
@@ -1,7 +0,0 @@
-var exec = require('child_process').exec;
-
-desc('Updates the documentation from GitHub');
-task('docs', [], function() {
-  exec('curl -O https://github.com/alexyoung/turing.js/raw/master/docs/index.html');
-});
-
